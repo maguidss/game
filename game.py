a86f9715afac79c01a99b3224efc76c213944b88
magda <magdacostaescola@gmail.com>
2023-03-16 14:56:24 +0000

    update

--- game.py
+++ game.py
@@ -1,2 +1,23 @@
-print("ola")
-gbjgjg
+import pygame 
+from sys import exit 
+
+pygame.init()
+screen = pygame.display.set_mode((500,600))
+pygame.display.set_caption('PuzzlePacked IQ Games')
+clock = pygame.time.Clock()
+
+score_surf = test_font.render('My game', False, 'Black')
+score_rect = score_surf.get_rect(center = (400, 50))
+level_surface = pygame.Surface((100,200))
+pygame.draw.rect(screen, 'Red',level_surface )
+#test_surface.fill('Red')
+
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    #screen.blit(test_surface, (200,100))
+    pygame.display.update()
+    clock.tick(60)

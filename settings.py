e7370efab48236a1670e10bfd4f9969d3ce86d26
sofiaam4 <128061518+sofiaam4@users.noreply.github.com>
2023-03-19 15:27:15 +0000

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,17 @@
+# colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (200, 200, 200)
+DARKGREY = (40, 40, 40)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BGCOLOUR = BLACK
+
+#game settings
+WIDTH = 1100
+HEIGHT = 641
+FPS= 60
+title = "PuzzlePacked IQ Games"
+TILESIZE = 50
+GAME_SIZE = 4
